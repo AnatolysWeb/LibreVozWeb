20291d2d9c5ce192a632c59940501485161d95b5
Yurquina Antonio <antonioyurquina.10@gmail.com>
2025-03-21 22:26:50 -0300

    Frontend terminado

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("post-form")?.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("Publicación enviada (aún falta el backend)");
+    });
+});
